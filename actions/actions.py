c3de4849e533cb552ee146c1a1fc423dc0f09a58
Specter613 <luisagh26@gmail.com>
2024-05-12 21:41:57 -0600

    Ya quedo

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import json
 import requests
+import urllib3
 
 class Buscar(Action):
 
@@ -12,10 +13,12 @@ class Buscar(Action):
     def run(self, dispatcher: CollectingDispatcher, 
             tracker: Tracker, 
             domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
+        urllib3.disable_warnings()
         informacion = tracker.get_slot("informacion")##Busca el tema en el url de abajo
-        url = 'https://proyectobotsistemas.000webhostapp.com/LearnAiBot/BuscarTema.php'#Aqui tambien va la url de nuestra BD 
+        # url = 'https://proyectobot.test/Tema.php'#Aqui tambien va la url de nuestra BD 
+        url = 'https://proyectobot2.000webhostapp.com/ProyectoBot/Tema.php'#Aqui tambien va la url de nuestra BD 
         params = {'tema': informacion}## Manda parametros al URL
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params,verify=False)
 
 
         #SI LA CONEXION AL SERVIDOR FUE EXITOSA EJECUTA EL SIGUIENTE BLOQUE DE CODIGO
@@ -23,10 +26,11 @@ class Buscar(Action):
             respuesta_php = response.text
             json_objeto = json.loads(respuesta_php)
             tema = json_objeto[0]["Capitulo"]## Extrae variable del capitulo
-            id = json_objeto[0]["Id"]
-            url2 = 'https://proyectobotsistemas.000webhostapp.com/LearnAiBot/informacion.php' #en espera para ver que es este link
+            id = json_objeto[0]["Id tema"]
+            # url2 = 'https://proyectobot.test/Informacion.php' #en espera para ver que es este link
+            url2 = 'https://proyectobot2.000webhostapp.com/ProyectoBot/Informacion.php' #en espera para ver que es este link
             params = {'cap' : tema, 'id':id}
-            response2= requests.get(url2, params=params)
+            response2= requests.get(url2, params=params,verify=False)
 
             if response.status_code == 200:
                 respuesta_php2 = response2.text
@@ -35,70 +39,71 @@ class Buscar(Action):
                 #AQUI SE DECLARAN LAS COLUMNSA DE NUESTRA BASE DE DATOS PARA ACCEDER A ELLAS COMO SI FUERAN MATRICES
 
                 conceptobreve = respuesta_json2[0]["Conceptobreve"]     
-                linkPag = respuesta_json2[0]["LinkPagina de referencia"]
+                linkPag = respuesta_json2[0]["Linkpagina"]
 
                 #ESTA ES LA SEGUNDA SINTAXIS PARA DECLARAR LAS COLUMNAS, MUY IMPORTANTE ESTA SINTAXIS YA QUE SIN ELLA NO SE DELCARAN LAS ANTERIORES
 
                 dispatcher.utter_message(text="{}".format(conceptobreve))
                 dispatcher.utter_message(text="{}".format(linkPag))
-                dispatcher.utter_message(text="Te puedo ayudar en algo mas?")
 
             else:
                 response.raise_for_status()
         else:
-           dispatcher.utter_message(text="MENSAJE POR SI EL BOT NO ENCONTRO NINGUN CONCEPTO RELACIONADO CON EL TEMA")
-           dispatcher.utter_message(text="SEGUNDO MENSAJE DE FALLA AL ENCONTRAR CONCEPTO")
+           dispatcher.utter_message(text="Lo siento al parecer no tengo informacion sobre eso")
+           dispatcher.utter_message(text="Lamento decirte que no tengo esa informacion")
 
         return []
       
     
 
-class Buscar_img(Action):
+    class Buscar_img(Action):
 
-    def name(self) -> Text:
-        return "Buscar_img"
-    
-    def run(self, dispatcher: CollectingDispatcher, 
-            tracker: Tracker, 
-            domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
-        informacion = tracker.get_slot("informacion")##Busca el tema en el url de abajo
-        url = 'https://proyectobotsistemas.000webhostapp.com/LearnAiBot/BuscarTema.php'#Aqui tambien va la url de nuestra BD 
-        params = {'tema': informacion}## Manda parametros al URL
-        response = requests.get(url, params=params)
+        def name(self) -> Text:
+            return "Buscar_img"
+        
+        def run(self, dispatcher: CollectingDispatcher, 
+                tracker: Tracker, 
+                domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
+            urllib3.disable_warnings()
+            informacion = tracker.get_slot("informacion")##Busca el tema en el url de abajo
+            # url = 'https://proyectobot.test/Tema.php'
+            url = 'https://proyectobot2.000webhostapp.com/ProyectoBot/Tema.php'#Aqui tambien va la url de nuestra BD 
+            params = {'tema': informacion}## Manda parametros al URL
+            response = requests.get(url, params=params,verify=False)
 
 
-        #SI LA CONEXION AL SERVIDOR FUE EXITOSA EJECUTA EL SIGUIENTE BLOQUE DE CODIGO
-        if response.status_code < 400 and response.text != '0':
-            respuesta_php = response.text
-            json_objeto = json.loads(respuesta_php)
-            tema = json_objeto[0]["Capitulo"]## Extrae variable del capitulo
-            id = json_objeto[0]["Id"]
-            url2 = 'https://proyectobotsistemas.000webhostapp.com/LearnAiBot/referenciasgraficas.php' #en espera para ver que es este link
-            params = {'cap' : tema, 'id':id}
-            response2= requests.get(url2, params=params)
+            #SI LA CONEXION AL SERVIDOR FUE EXITOSA EJECUTA EL SIGUIENTE BLOQUE DE CODIGO
+            if response.status_code < 400 and response.text != '0':
+                respuesta_php = response.text
+                json_objeto = json.loads(respuesta_php)
+                tema = json_objeto[0]["Capitulo"]## Extrae variable del capitulo
+                id = json_objeto[0]["Id tema"]
+                # url2 = 'https://proyectobot.test/Imagenes.php'
+                url2 = 'https://proyectobot2.000webhostapp.com/ProyectoBot/Imagenes.php' #en espera para ver que es este link
+                params = {'cap' : tema, 'id':id}
+                response2= requests.get(url2, params=params,verify=False)
 
-            if response.status_code == 200:
-                respuesta_php2 = response2.text
-                respuesta_json2 = json.loads(respuesta_php2)
+                if response.status_code == 200:
+                    respuesta_php2 = response2.text
+                    respuesta_json2 = json.loads(respuesta_php2)
 
-                #AQUI SE DECLARAN LAS COLUMNSA DE NUESTRA BASE DE DATOS PARA ACCEDER A ELLAS COMO SI FUERAN MATRICES
+                    #AQUI SE DECLARAN LAS COLUMNSA DE NUESTRA BASE DE DATOS PARA ACCEDER A ELLAS COMO SI FUERAN MATRICES
 
-                linkvid = respuesta_json2[0]["Link para video"]
-                imagenReferencia = respuesta_json2[0]["ImagenReferencia"]
+                    linkvid = respuesta_json2[0]["Video"]
+                    imagenReferencia = respuesta_json2[0]["Imagen"]
 
-                #ESTA ES LA SEGUNDA SINTAXIS PARA DECLARAR LAS COLUMNAS, MUY IMPORTANTE ESTA SINTAXIS YA QUE SIN ELLA NO SE DELCARAN LAS ANTERIORES
+                    #ESTA ES LA SEGUNDA SINTAXIS PARA DECLARAR LAS COLUMNAS, MUY IMPORTANTE ESTA SINTAXIS YA QUE SIN ELLA NO SE DELCARAN LAS ANTERIORES
 
-                dispatcher.utter_message(text="{}".format(linkvid))
-                dispatcher.utter_message(text="{}".format(imagenReferencia))
-                dispatcher.utter_message(text="Necesitas alguna nueva consulta?")
+                    dispatcher.utter_message(text="{}".format(linkvid))
+                    dispatcher.utter_message(text="{}".format(imagenReferencia))
 
+                else:
+                    response.raise_for_status()
             else:
-                response.raise_for_status()
-        else:
-           dispatcher.utter_message(text="MENSAJE POR SI EL BOT NO ENCONTRO NINGUN CONCEPTO RELACIONADO CON EL TEMA")
-           dispatcher.utter_message(text="SEGUNDO MENSAJE DE FALLA AL ENCONTRAR CONCEPTO")
+                dispatcher.utter_message(text="Lo siento al parecer no tengo informacion sobre eso")
+                dispatcher.utter_message(text="Lamento decirte que no tengo esa informacion")
 
-        return []
+            return []
 
 
 class ActionDefaultFallback(Action):
